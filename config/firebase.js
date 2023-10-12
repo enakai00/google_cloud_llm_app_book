7c1cc343e8202238590d8a097608b073fb12436b
Etsuji Nakai <enakai0@gmail.com>
2023-10-12 11:13:32 +0000

    Update

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC1zdAsPFjMtj64JXNJcz1DMY4EtWKSaKU",
-  authDomain: "etsuji-react-example-379705.firebaseapp.com",
-  projectId: "etsuji-react-example-379705",
-  storageBucket: "etsuji-react-example-379705.appspot.com",
-  messagingSenderId: "417550172325",
-  appId: "1:417550172325:web:b616688540489089f50d4c"
-};
+import { firebaseConfig } from "../.firebase";
 
 export var app;
 
