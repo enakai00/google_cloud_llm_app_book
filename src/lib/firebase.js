42400ced247f55dfc87ce7cea78048325f080b78
Etsuji Nakai <enakai0@gmail.com>
2023-10-15 23:33:50 +0000

    Update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,7 +6,7 @@ try {
   initializeApp(firebaseConfig);
 } catch (err) {
   if (!/already exists/.test(err.message)) {
-    console.error('Firebase initialization error', err.stack)
+    console.error('Firebase initialization error', err.stack);
   }
 }
 
