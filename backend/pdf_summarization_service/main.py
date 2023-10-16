3ddc45ea4afaa081d3d6e433dddb9e3bb9531318
Etsuji Nakai <enakai0@gmail.com>
2023-10-16 07:40:57 +0000

    Update

--- backend/pdf_summarization_service/main.py
+++ backend/pdf_summarization_service/main.py
@@ -37,18 +37,18 @@ def upload_to_gcs(bucket_name, filepath, filename):
 def process_event():
     event = from_http(request.headers, request.data)
     data = event.data
-    bucket_name = data["bucket"]
-    filepath = data["name"]
+    bucket_name = data['bucket']
+    filepath = data['name']
 
     directory = os.path.dirname(filepath)
     filename = os.path.basename(filepath)
     filename_body, ext = os.path.splitext(filename)
     new_filepath = directory + '/summary/' + filename_body + '.txt'
 
-    print("Uploaded file: {}".format(filepath))
+    print('Uploaded file: {}'.format(filepath))
 
     if not ext.lower() == '.pdf':
-        return ("This is not a pdf file", 200)
+        return ('This is not a pdf file', 200)
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         tmp_file = os.path.join(tmp_dir, filename)
@@ -64,20 +64,20 @@ def process_event():
 
         qa_chain = load_qa_chain(llm, chain_type='map_reduce')
         qa_document_chain = AnalyzeDocumentChain(combine_docs_chain=qa_chain)
-        prompt = "何についての文書ですか？日本語で5文以内にまとめてください。"
+        prompt = '何についての文書ですか？日本語で5文以内にまとめてください。'
         description = qa_document_chain.run(
             input_document=document[0].page_content[:5000],
             question=prompt)
 
-    print("Description of {}: {}".format(filename, description))
-    with tempfile.NamedTemporaryFile() as t:
-        with open(t.name, 'w') as f:
+    print('Description of {}: {}'.format(filename, description))
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        with open(tmp_file.name, 'w') as f:
             f.write(description)
-        upload_to_gcs(bucket_name, new_filepath, t.name)
+        upload_to_gcs(bucket_name, new_filepath, tmp_file.name)
 
-    return ("succeeded", 200)
+    return ('succeeded', 200)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
