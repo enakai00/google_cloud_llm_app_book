112aa8906f311bfc442396616a5d3668880302a2
Etsuji Nakai <enakai0@gmail.com>
2023-10-17 08:25:28 +0000

    Update

--- src/components/filestore.js
+++ src/components/filestore.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { auth } from "lib/firebase";
+import { getStorage, getBlob, ref, listAll } from "firebase/storage";
 
 
 export default function Filestore() {
@@ -13,28 +14,72 @@ export default function Filestore() {
   useEffect(() => { getFileList(); }, []);
 
   const getFileList = async () => {
-    const newFileList = ['aaaaa.pdf', 'bbbbb.pdf', 'ccccc.pdf'];
+    const storage = getStorage();
+    const uid = auth.currentUser.uid;
+    let listRef = ref(storage, uid + "/summary");
+    let res = await listAll(listRef);
+    const summaryList = [];
+    for (let item of res.items) {
+      summaryList.push(item.name.replace(/(.txt$)/, ".pdf"));
+    }
+
+    listRef = ref(storage, uid);
+    res = await listAll(listRef);
+    const newFileList = [];
+    for (let item of res.items) {
+      let summary = false;
+      if (summaryList.includes(item.name)) {
+        summary = true;        
+      }
+      newFileList.push({filename: item.name, summary: summary});
+    }
+
     setFileList(newFileList);
   };
 
-  const showSummary = async () => {
-    setPopupText("aaaaa\nbbbbbbb");
+  const showSummary = async (filename) => {
+    setPopupText("Loading...");
     setShowPopup(true);
+    const storage = getStorage();
+    const uid = auth.currentUser.uid;
+    const filepath = uid + "/summary/" + filename.replace(/(.pdf$)/, ".txt");
+    const summaryBlob = await getBlob(ref(storage, filepath));
+    const summaryText = await summaryBlob.text();
+    setPopupText(summaryText);
   }
+
+  const infoButton = (item) => {
+    var buttonElement;
+    if (item.summary) {	  	
+      buttonElement = (
+        <span className="circle" style={{cursor: "pointer"}}
+	      onClick={() => showSummary(item.filename)}>i</span>
+      );
+    } else {    
+      buttonElement = (
+        <span className="circle" style={{backgroundColor: "#fff"}}> </span>
+      );
+    }
+    return buttonElement;
+  };
 
   const fileListElement = [];
   for (let item of fileList) {
     const fileElement = (
-      <div key={item} onClick={showSummary}>{item}</div>
+      <div key={item.filename}
+           style={{height: "1.8rem", lineHeight: "1.8rem"}}>
+	{infoButton(item)} {item.filename}
+      </div>
     );
     fileListElement.push(fileElement);
   }
 
   const popupElement = (
-    <div style={{
-      position: "absolute", left: "100px", top: "50px",
-      width: "400px", height: "300px", padding: "10px", margin: "10px",
-      border: "1px solid", borderRadius: "10px", backgroundColor: "#f0f0f0"}}>
+    <div style={{position: "absolute", left: "100px", top: "50px",
+                 width: "400px", height: "300px",
+                 padding: "10px", margin: "10px",
+                 border: "1px solid", borderRadius: "10px",
+		 backgroundColor: "#f0f0f0"}}>
       {popupText}
       <div style={{position: "absolute", bottom: "10px", right: "10px"}}>
         <button onClick={() => setShowPopup(false)}>Close</button>
